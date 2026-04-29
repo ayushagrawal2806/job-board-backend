b08b66ebc01d64688d07135104c8d9fbd92722de
Ayush Agrawal <ayushagrawal6082@gmail.com>
2026-04-29 15:28:05 +0530

    Add error logging to email event listeners for debugging

--- src/main/java/com/ayush/jobboard/listner/ApplicationEventListener.java
+++ src/main/java/com/ayush/jobboard/listner/ApplicationEventListener.java
@@ -4,12 +4,14 @@ import com.ayush.jobboard.event.ApplicationCreatedEvent;
 import com.ayush.jobboard.event.StatusUpdatedEvent;
 import com.ayush.jobboard.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ApplicationEventListener {
@@ -20,38 +22,43 @@ public class ApplicationEventListener {
     @EventListener
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleApplicationCreated(ApplicationCreatedEvent event) {
-        var application = event.getApplication();
-        var applicant = application.getApplicant();
-        var job = application.getJob();
+       try {
+           var application = event.getApplication();
+           var applicant = application.getApplicant();
+           var job = application.getJob();
 
 
-        emailService.sendEmail(
-                applicant.getEmail(),
-                "Application Received – " + job.getTitle(),
-                "Hi " + applicant.getName() + ",\n\n" +
-                        "Great news! Your application for the position of " + job.getTitle() + " at " + job.getCompanyName() + " has been successfully received.\n\n" +
-                        "Our team will review your application and get back to you shortly.\n\n" +
-                        "Best of luck!\n" +
-                        "Team CareerPulse"
-        );
+           emailService.sendEmail(
+                   applicant.getEmail(),
+                   "Application Received – " + job.getTitle(),
+                   "Hi " + applicant.getName() + ",\n\n" +
+                           "Great news! Your application for the position of " + job.getTitle() + " at " + job.getCompanyName() + " has been successfully received.\n\n" +
+                           "Our team will review your application and get back to you shortly.\n\n" +
+                           "Best of luck!\n" +
+                           "Team CareerPulse"
+           );
 
 
-        emailService.sendEmail(
-                job.getRecruiter().getEmail(),
-                "New Applicant for " + job.getTitle(),
-                "Hi,\n\n" +
-                        "You have a new applicant for your job posting – " + job.getTitle() + ".\n\n" +
-                        "Applicant: " + applicant.getName() + "\n" +
-                        "Email: " + applicant.getEmail() + "\n\n" +
-                        "Login to CareerPulse to review their application and resume.\n\n" +
-                        "Team CareerPulse"
-        );
+           emailService.sendEmail(
+                   job.getRecruiter().getEmail(),
+                   "New Applicant for " + job.getTitle(),
+                   "Hi,\n\n" +
+                           "You have a new applicant for your job posting – " + job.getTitle() + ".\n\n" +
+                           "Applicant: " + applicant.getName() + "\n" +
+                           "Email: " + applicant.getEmail() + "\n\n" +
+                           "Login to CareerPulse to review their application and resume.\n\n" +
+                           "Team CareerPulse"
+           );
+       }catch (Exception e){
+           log.error("Failed to send application created email: {}", e.getMessage(), e);
+       }
     }
 
     @Async
     @EventListener
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleStatusUpdated(StatusUpdatedEvent event) {
+    try  {
         var application = event.getApplication();
         var applicant = application.getApplicant();
         var job = application.getJob();
@@ -66,5 +73,8 @@ public class ApplicationEventListener {
                         "Best regards,\n" +
                         "Team CareerPulse"
         );
+    } catch (Exception e) {
+        log.error("Failed to send status updated email: {}", e.getMessage(), e);
+    }
     }
 }
